76e23f54e75fcfe69d2a3811ccb38580bcca8446
Adroaldo de Andrade <adroaldof@gmail.com>
2016-06-30 09:30:22 -0300

    Add initial configuration files

---
+++ README.md
@@ -0,0 +1,56 @@
+# Generic Server
+
+This project is a generic server sample.
+
+## Start Using
+
+To work on this project follow the rules
+
+**Clone the repo**
+```bash
+git clone git@github.com:adroaldof/generic-app-server.git
+cd generic-app-server
+```
+
+**Install global dependencies**
+```bash
+npm install -g gulp-cli
+```
+
+
+**Install project dependencies**
+```bash
+npm install
+```
+
+And you are good to go. Checkout the commands bellow and enjoy :)
+
+
+## Useful Commands
+
+Here are some shortcut commands that I use
+
+| Command | Description |
+| :---    | :---        |
+| `npm run serve` | Run local server with live reload on change |
+| `npm run full-serve` | Run test, documentation and start local server with live reload on change |
+| `npm run tests` | Run tests and generate `coverage` directory results |
+| `npm run lint` | Lint JS files (JSHint and JSCS) |
+| `npm run documentate` | Generate documentation and locate at `docs` folder |
+| `npm run clean` | Clean all generated files (`docs`, `dist`) |
+| `npm run pre` | Crete a Git Tag with pre release (E.g. 0.0.0-1) |
+| `npm run patch` | Crete a Git Tag with patch release (E.g. 0.0.1) |
+| `npm run minor` | Crete a Git Tag with minor release (E.g. 0.1.0) |
+| `npm run major` | Crete a Git Tag with major release (E.g. 1.0.0) |
+| `npm run help` | Show help command options |
+
+
+## Some References
+
+The follow references were used to build this project. Thank you so much guys +1
+
+- [Kunal Kapadia - express-mongoose-es6-rest-api](https://github.com/KunalKapadia/express-mongoose-es6-rest-api)
+- [Jason Miller - express-es6-rest-api](https://github.com/developit/express-es6-rest-api)
+- [Pranav Sathyanarayanan - node-backend-boilerplate](https://github.com/PranavSathy/node-backend-boilerplate)
+- [Madhusudhan Srinivasa - node-express-mongoose-demo](https://github.com/madhums/node-express-mongoose-demo)
+- [Sahat Yalkabov - hackathon-starter](https://github.com/sahat/hackathon-starter)
---
+++ gulpfile.babel.js
@@ -0,0 +1,467 @@
+// 'use strict';
+
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import path from 'path';
+import babelCompiler from 'babel-core/register';
+import * as isparta from 'isparta';
+import argv from 'yargs';
+import del from 'del';
+import fs from 'graceful-fs';
+import runSequence from 'run-sequence';
+
+
+const plugins = gulpLoadPlugins();
+const releaser = require('conventional-github-releaser');
+
+const paths = {
+          configs: {
+              changelog: './CHANGELOG.md',
+              gitignore: './.gitignore',
+              jscsrc: './.jscsrc',
+              jshintrc: './.jshintrc',
+              package: './package.json'
+          },
+          dirs: {
+              base: './',
+              coverage: './coverage',
+              lib: './lib',
+              docs: {
+                  base: './docs',
+                  doc: './docs/doc',
+                  api: './docs/api'
+              },
+              public: './public',
+              src: './src'
+          },
+          files: {
+              js: [
+                  './src/**/*.js',
+                  '!./src/**/*.tests.js',
+                  '!./lib/**/*',
+                  '!./node_modules/**/*',
+                  '!./coverage/**/*',
+                  '!gulpfile.babel.js'
+              ],
+              tests: './src/**/*.tests.js'
+          },
+          jslint: [
+              './gulpfile.js',
+              './src/**/*.js',
+              '!./lib/**/*.js'
+          ]
+      };
+
+const options = {
+          codeCoverage: {
+              reporters: ['lcov', 'text-summary'],
+              thresholds: {
+                  global: {
+                      statements: 80,
+                      branches: 80,
+                      functions: 80,
+                      lines: 80,
+                  },
+                  each: {
+                      statements: 50,
+                      branches: 50,
+                      functions: 50,
+                      lines: 50,
+                  }
+              }
+          }
+      };
+
+
+/************************************************************************
+ * Clean files
+ ************************************************************************/
+
+gulp.task('clean', () => {
+    const pathsToDelete = [
+        paths.dirs.lib
+    ];
+
+    return del(pathsToDelete);
+});
+
+
+gulp.task('clean-full', () => {
+    const pathsToDelete = [
+        paths.dirs.docs.base,
+        paths.dirs.coverage,
+        paths.dirs.lib
+    ];
+
+    return del(pathsToDelete);
+});
+
+
+/************************************************************************
+ * Copy non JS Files
+ ************************************************************************/
+
+gulp.task('copy', () => {
+    gulp.src([paths.configs.gitignore, paths.configs.package])
+        .pipe(plugins.newer(paths.dirs.lib))
+        .pipe(gulp.dest(paths.dirs.lib));
+});
+
+
+/************************************************************************
+ * Documentation
+ ************************************************************************/
+
+gulp.task('document', () => {
+    return gulp.src(paths.dirs.src)
+        .pipe(plugins.esdoc({destination: paths.dirs.docs.doc}));
+});
+
+
+gulp.task('apidoc', (done) => {
+    const options = {
+        src: paths.dirs.src,
+        dest: paths.dirs.docs.api,
+        verbose: true,
+        includeFilters: [ ".*\\.js$" ]
+    };
+
+    plugins.apidoc(options, done);
+});
+
+
+/************************************************************************
+ * Linters (jshint, jscs)
+ ************************************************************************/
+
+gulp.task('jshint', () => {
+    return gulp.src(paths.jslint)
+        .pipe(plugins.jshint(paths.configs.jshintrc))
+        .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+
+gulp.task('jscs', () => {
+    return gulp.src(paths.jslint)
+        .pipe(plugins.jscs(paths.configs.jscsrc))
+        .pipe(plugins.jshint.reporter('jscs-stylish'));
+});
+
+
+/************************************************************************
+ * Transpiler
+ ************************************************************************/
+
+gulp.task('transpile', () => {
+    return gulp.src(paths.files.js, {base: paths.dirs.src})
+        .pipe(plugins.newer(paths.dirs.lib))
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.babel())
+        .pipe(plugins.sourcemaps.write('.', {
+            includeContent: false,
+            sourceRoot (file) {
+                return path.relative(file.path, __dirname);
+            }
+        }))
+        .pipe(gulp.dest(paths.dirs.lib));
+});
+
+
+/************************************************************************
+ * Tests
+ ************************************************************************/
+
+/**
+ * Set node environment variable to test
+ */
+gulp.task('set-env', () => {
+    plugins.env({
+        vars: {
+            NODE_ENV: 'test'
+        }
+    });
+});
+
+
+/**
+ * Convert files to code coverage
+ */
+gulp.task('pre-test', () => {
+    return gulp.src([...paths.files.js])
+        .pipe(plugins.istanbul({
+            instrumenter: isparta.Instrumenter,
+            includeUntested: true
+        }))
+        .pipe(plugins.istanbul.hookRequire());
+});
+
+
+/**
+ * Start mocha tests
+ */
+gulp.task('run-test', ['pre-test', 'set-env'], () => {
+    let reporters;
+    let exitCode = 0;
+
+    if (plugins.util.env['code-coverage-reporter']) {
+        reporters = [
+            ...options.codeCoverage.reporters,
+            plugins.util.env['code-coverage-reporter']
+        ];
+    } else {
+        reporters = options.codeCoverage.reporters;
+    }
+
+    return gulp.src([paths.files.tests], { read: false })
+        .pipe(plugins.plumber())
+        .pipe(plugins.mocha({
+            reporter: plugins.util.env['mocha-reporter'] || 'spec',
+            colors: true,
+            ui: 'bdd',
+            timeout: 6000,
+            compilers: { js: babelCompiler }
+        }))
+        .pipe(plugins.istanbul.writeReports({
+            dir: paths.dirs.coverage,
+            reporters
+        }))
+        .pipe(plugins.istanbul.enforceThresholds({
+            thresholds: options.codeCoverage.thresholds
+        }))
+        .once('error', (err) => {
+            plugins.util.log(err);
+            exitCode = 1;
+        })
+        .once('end', () => {
+            plugins.util.log('*** Tests Completed ***');
+            process.exit(exitCode);
+        });
+});
+
+
+/**
+ * Execute tests
+ * This will clean, transpile, copy and run tests
+ */
+gulp.task('test', ['clean'], () => {
+    runSequence(
+        ['copy', 'transpile'],
+        'run-test'
+    );
+});
+
+
+/************************************************************************
+ * Release
+ ************************************************************************/
+
+gulp.task('nodemon', () => {
+    plugins.nodemon({
+        script: path.join(paths.dirs.lib, 'index.js'),
+        ext: 'js',
+        verbose: true,
+        watch: [
+            paths.dirs.docs.base,
+            paths.dirs.coverage,
+            paths.dirs.public,
+            paths.dirs.src
+        ],
+        ignore: [
+            'node_module/',
+            'lib/',
+            'gulpfile.babe.js'
+        ],
+        tasks: ['transpile']
+    });
+});
+
+
+/************************************************************************
+ * Release
+ ************************************************************************/
+
+function releaseTask (type) {
+    console.log('releaseTask', type);
+    gulp.src(paths.configs.package)
+        .pipe(plugins.bump({
+            type: type
+        }))
+        .pipe(gulp.dest(paths.dirs.base));
+}
+
+gulp.task('pre-release', () => {
+    releaseTask('prerelease');
+});
+
+
+gulp.task('patch-release', () => {
+    releaseTask('patch');
+});
+
+
+gulp.task('minor-release', () => {
+    releaseTask('minor');
+});
+
+
+gulp.task('major-release', () => {
+    releaseTask('major');
+});
+
+
+gulp.task('changelog', () => {
+    console.log('changelog');
+    return gulp.src(paths.configs.changelog)
+    .pipe(plugins.conventionalChangelog())
+    .pipe(gulp.dest(paths.dirs.base));
+});
+
+
+gulp.task('github-release', (done) => {
+    releaser({
+        type: 'oauth',
+        token: 'GITHUB_ACCESS_TOKEN'
+    }, {
+        preset: 'Releasing Version'
+    }, done);
+});
+
+
+gulp.task('commit-changes', () => {
+    function getPackageJsonVersion () {
+        return JSON.parse(fs.readFileSync(paths.configs.package, 'utf8')).version;
+    }
+    console.log('commit changes');
+
+    return gulp.src('.')
+        .pipe(plugins.git.add())
+        .pipe(plugins.git.commit('New tag and bumped ' + argv.type + ' version number v' + getPackageJsonVersion()));
+});
+
+
+gulp.task('push-changes', (cb) => {
+    console.log('push-changes');
+    plugins.git.push('origin', 'master', cb);
+});
+
+
+gulp.task('create-new-tag', (cb) => {
+    function getPackageJsonVersion () {
+        return JSON.parse(fs.readFileSync(paths.configs.package, 'utf8')).version;
+    }
+
+    const version = getPackageJsonVersion();
+
+    plugins.git.tag(String('v').concat(version), 'Created Tag for version: v' + version, (error) => {
+        if (error) {
+            return cb(error);
+        }
+        console.log('tag version ' + version);
+
+        plugins.git.push('origin', 'master', {args: '--tags'}, cb);
+    });
+});
+
+
+gulp.task('release', () => {
+    function releaseType () {
+        console.log('type', argv.type);
+        switch (argv.type) {
+            case 'pre':
+                return 'pre-release';
+            case 'patch':
+                return 'patch-release';
+            case 'minor':
+                return 'minor-release';
+            case 'major':
+                return 'major-release';
+            default:
+                console.log('You can choose release type among "pre|patch|minor|major"');
+                console.log('Using now "patch"');
+                return 'patch-release';
+        }
+    }
+
+
+    function callBack (error) {
+        if (error) {
+            console.log(error.message);
+        } else {
+            console.log('RELEASE FINISHED SUCCESSFULLY');
+        }
+
+        return false;
+    }
+
+
+    runSequence(
+        releaseType(),
+        'changelog',
+        'commit-changes',
+        'push-changes',
+        'create-new-tag',
+        'github-release',
+        callBack
+    );
+});
+
+
+/************************************************************************
+ * Grouping Tasks
+ *************************************************************************/
+
+gulp.task('documentate', [
+    'document',
+    'apidoc'
+]);
+
+
+gulp.task('build', [
+    'transpile',
+    'documentate'
+]);
+
+
+gulp.task('lint', [
+    'jshint',
+    'jscs'
+]);
+
+
+gulp.task('default', [
+    'lint',
+    'build'
+]);
+
+gulp.task('serve', ['clean'], () => {
+    runSequence('copy', 'transpile', 'nodemon');
+});
+
+gulp.task('full-serve', () => {
+    runSequence(
+        'clean-full',
+        'lint',
+        'transpile',
+        'test',
+        'documentate',
+        'nodemon'
+    );
+});
+
+gulp.task('help', () => {
+    plugins.util.log('================================================================================');
+    plugins.util.log('Available commands');
+    plugins.util.log('================================================================================');
+    plugins.util.log('| gulp serve       | npm run serve       | Run local server with live reload');
+    plugins.util.log('| gulp full-serve  | npm run full-serve  | Run test, documentation and server');
+    plugins.util.log('| gulp test        | npm run test        | Run tests and generate `coverage`');
+    plugins.util.log('| gulp lint        | npm run lint        | Lint JS files (JSHint and JSCS)');
+    plugins.util.log('| gulp documentate | npm run documentate | Generate documentation at `docs`');
+    plugins.util.log('| gulp clean       | npm run clean       | Clean `docs`, `coverage` and `lib`');
+    plugins.util.log('| gulp pre         | npm run pre         | Git Tag pre release (E.g. 0.0.0-1)');
+    plugins.util.log('| gulp patch       | npm run patch       | Git Tag patch release (E.g. 0.0.1)');
+    plugins.util.log('| gulp minor       | npm run minor       | Git Tag minor release (E.g. 0.1.0)');
+    plugins.util.log('| gulp major       | npm run major       | Git Tag major release (E.g. 0.1.0)');
+    plugins.util.log('| gulp help        | npm run help        | Show this help options');
+    plugins.util.log('================================================================================');
+});
