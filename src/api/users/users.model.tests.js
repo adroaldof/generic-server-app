88de52f613e0367b91309582d8794d0da4f5d300
Adroaldo de Andrade <adroaldof@gmail.com>
2016-07-14 22:14:23 -0300

    Try test user model authentication static function
    PS: it does’t work :(

--- src/api/users/users.model.tests.js
+++ src/api/users/users.model.tests.js
@@ -88,98 +88,31 @@ describe('User Model Tests', () => {
     });
 
 
-    // describe('User Model Tests', () => {
-    //     let savedUser = {};
+    // describe.only('Login User Tests', () => {
+    //     it('should test login user', (done) => {
+    //         let userObject = {
+    //             email: 'john-doe@gmail.com',
+    //             password: 'Passw0rd',
+    //             passwordSalt: 'SomePasswordSalt'
+    //         }
+    //         let expecteUser = _.clone(userObject);
+    //         _.unset(expecteUser, 'password');
+    //         _.unset(expecteUser, 'passwordSalt');
     //
-    //     it('should create an user', (done) => {
-    //         const user = new User({
-    //             name: 'Test User',
-    //             email: 'test@gmail.com',
-    //             mobileNumber: '4987658765'
-    //         });
+    //         const user = sinon.mock(User);
     //
-    //         user.save((err, user) => {
-    //             if (err) {
-    //                 expect(err).to.be.null;
-    //             }
+    //         user.expects('authenticate')
+    //             .once()
+    //             .withArgs(userObject.email, userObject.password, expecteUser);
     //
-    //             expect(user.email).to.equal('test@gmail.com');
-    //             expect(user).to.have.property('createdAt');
-    //             expect(user).to.have.property('updatedAt');
-    //             savedUser = user;
+    //         user.authenticate(userObject.email, userObject.password, (err, user) => {
+    //             console.log('err', err);
+    //             console.log('user', user);
     //             done();
     //         });
-    //     });
     //
-    //     it('should not create a user with duplicated email', (done) => {
-    //         const user = new User({
-    //             name: 'Another Test User with Same Email',
-    //             email: 'test@gmail.com',
-    //             mobileNumber: '4887658765'
-    //         });
-    //
-    //         user.save((err) => {
-    //             expect(err).to.be.defined;
-    //             expect(err.code).to.equal(11000);
-    //             done();
-    //         });
-    //     });
-    //
-    //     it('should retrieve one saved user', (done) => {
-    //         User.get(savedUser._id)
-    //             .then((user) => {
-    //                 expect(user.name).to.equal(savedUser.name);
-    //                 expect(user.email).to.equal(savedUser.email);
-    //                 expect(user.mobileNumber).to.equal(savedUser.mobileNumber);
-    //                 done();
-    //             }, (err) => {
-    //                 expect(err).to.be.null;
-    //                 done();
-    //             });
-    //     });
-    //
-    //     it('should not retrieve an user that not exists', (done) => {
-    //         User.get('57676538ff7cdcaade01eccc')
-    //             .then((user) => {
-    //                 expect(user).to.be.null;
-    //                 done();
-    //             }, (err) => {
-    //                 expect(err.name).to.equal('Error');
-    //                 expect(err.status).to.equal(404);
-    //                 done();
-    //             });
-    //     });
-    //
-    //     it('should retrieve all saved users', (done) => {
-    //         User.list()
-    //             .then((users) => {
-    //                 expect(users).to.an.object;
-    //                 expect(users.length).to.be.above(0);
-    //                 done();
-    //             }, (err) => {
-    //                 expect(err).to.be.null;
-    //             });
-    //     });
-    //
-    //     it('should find user by email', (done) => {
-    //         User.findOne({ email: 'test@gmail.com' }, (err, user) => {
-    //             if (err) {
-    //                 expect(err).to.be.null;
-    //             }
-    //
-    //             expect(user.email).to.equal('test@gmail.com');
-    //             done();
-    //         });
-    //     });
-    //
-    //     it('should delete a user', (done) => {
-    //         User.remove({ email: 'test@gmail.com' }, (err) => {
-    //             if (err) {
-    //                 expect(err).to.be.null;
-    //             }
-    //
-    //             done();
-    //         });
+    //         user.verify();
+    //         user.restore();
     //     });
     // });
 
