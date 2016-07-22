68cced6ca4c702f216597b7ba86d7410cf36d509
Adroaldo de Andrade <adroaldof@gmail.com>
2016-07-22 19:00:02 -0300

    Fix location on auth area

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -7,7 +7,7 @@ import response from '../../helpers/response-formatter/response-formatter';
 const router = express.Router();
 
 router.route('/')
-    .get(authController.signout, response.load({}, 'index'), response.send)
+    .get(authController.signout, response.load({}, '/'), response.send)
     .post(authController.signin, response.send);
 
 export default router;
