4961bb3fcc248ff5d31f8f6e6e5752112b6add4a
Adroaldo de Andrade <adroaldof@gmail.com>
2016-07-24 21:39:25 -0300

    Lint and code fixes on token generator

--- src/helpers/token/token.js
+++ src/helpers/token/token.js
@@ -3,21 +3,22 @@ import crypto from 'crypto';
 const LEN = 16;
 
 function generateToken (randomBytes, callback) {
-    if (typeof randomBytes === 'function') {
-        callback = randomBytes;
-        randomBytes = LEN;
+    let randomBytesSize = randomBytes;
+
+    if (!randomBytes || randomBytesSize === null || typeof randomBytesSize !== 'number') {
+        randomBytesSize = LEN;
     }
 
-    randomBytes = randomBytes / 2;
+    randomBytesSize = randomBytesSize / 2;
 
-    crypto.randomBytes(randomBytes, (err, buffer) => {
+    crypto.randomBytes(randomBytesSize, (err, buffer) => {
         if (err) {
             return callback(err);
         }
 
         const token = buffer.toString('hex');
 
-        callback(null, token);
+        return callback(null, token);
     });
 }
 
