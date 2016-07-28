13c5b3209810f35f8f4ae0c30bbbc0ed303c784f
Adroaldo de Andrade <adroaldof@gmail.com>
2016-07-28 17:57:20 -0300

    Add some apidoc documentation data on auth module

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -3,6 +3,41 @@
 import _ from 'lodash';
 import passport from 'passport';
 
+/**
+ * @api {POST} /api/{version}/auth/ Sign in an user
+ * @apiName Signin
+ * @apiGroup Authentication
+ *
+ * @apiSuccess {Object} data Returns a logged user
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 Found
+ *     {
+ *          data: {
+ *              user: {
+ *                  _id: '578f947b57ad33a843b7eda2',
+ *                  name: 'John Doe',
+ *                  email: 'john-doe@gmail.com'
+ *              }
+ *          },
+ *          info: 'Successfully logged in',
+ *          status: 200,
+ *          page: '/user/578f947b57ad33a843b7eda2',
+ *          shouldRedirect: true
+ *     }
+ *
+ * @apiError UserNotFound User not found
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *          data: {
+ *              err: 'User not found',
+ *              status: 404
+ *          },
+ *          page: '/'
+ *     }
+ */
 
 function signin (req, res, next) {
     req.resources = req.resources || {};
@@ -29,6 +64,23 @@ function signin (req, res, next) {
         });
     })(req, res, next);
 }
+
+
+/**
+ * @api {GET} /api/{version}/logout Logout an user
+ * @apiName Signout
+ * @apiGroup Authentication
+ *
+ * @apiSuccess {Object} data Logout message
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 Found
+ *     {
+ *          info: 'Successfully logged out',
+ *          status: 200,
+ *          shouldRedirect: true
+ *     }
+ */
 
 function signout (req, res, next) {
     req.resources = req.resources || {};
